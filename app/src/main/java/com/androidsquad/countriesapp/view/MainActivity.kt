48d2f22bc68326ac0dc84ca8017e964652e12330
CarlosTrujilloTW <carlos.trujillo@thoughtworks.com>
2023-02-03 10:23:28 -0300

    se agrega bottombar

--- app/src/main/java/com/androidsquad/countriesapp/view/MainActivity.kt
+++ app/src/main/java/com/androidsquad/countriesapp/view/MainActivity.kt
@@ -8,30 +8,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.androidsquad.countriesapp.R
 import com.androidsquad.countriesapp.model.Country
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,24 +54,31 @@ fun TitleContainer(modifier: Modifier = Modifier) {
                 .padding(start = 16.dp, top = 16.dp))
     }*/
 
-            TopAppBar(
-                title = { Text(text = stringResource(R.string.title),
-                    fontSize = 30.sp,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                        .padding(start = 16.dp, top = 5.dp)) },
-
-                actions = {
-                    // RowScope here, so these icons will be placed horizontally
-                    IconButton(onClick = { /* doSomething() */ }) {
-                        Icon(Icons.Filled.Person, contentDescription = "Localized description")
-                    }
-                }
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.title),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                /*fontSize = 30.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.CenterHorizontally)
+                    .padding(start = 16.dp, top = 5.dp)*/
             )
+        },
+
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.AccountBox, contentDescription = "Account")
+            }
+        }
+    )
 }
+
 @Composable
-fun ListContainer(modifier: Modifier = Modifier){
+fun ListContainer(modifier: Modifier = Modifier) {
     val arrayOfCountries = arrayOf(
         Country("Ecuador1", 1),
         Country("Ecuador2", 2),
@@ -100,13 +97,14 @@ fun ListContainer(modifier: Modifier = Modifier){
         modifier = Modifier.verticalScroll(scrollState)
     )
     {
-        for (country in arrayOfCountries){
+        for (country in arrayOfCountries) {
             ItemContainer(country.name, country.image)
         }
     }
 }
+
 @Composable
-fun ItemContainer(name: String, img:Int, modifier: Modifier = Modifier){
+fun ItemContainer(name: String, img: Int, modifier: Modifier = Modifier) {
     Row(modifier = modifier) {
         Text(text = name)
         Image(
@@ -120,31 +118,34 @@ fun ItemContainer(name: String, img:Int, modifier: Modifier = Modifier){
         )
     }
 }
+
 @Composable
-fun ButtonContainer(modifier: Modifier = Modifier){
-    Row(modifier = modifier){
-        Button(onClick = { },
-            modifier = Modifier.padding(10.dp)) {
-            Text(text = "boton1")
+fun BottomContainer(modifier: Modifier = Modifier) {
+    BottomAppBar() {
+        Spacer(Modifier.weight(0.5f, true))
+        IconButton(onClick = { /*TODO*/ }) { // (3)
+            Icon(imageVector = Icons.Filled.Home, contentDescription = "Buscar")
         }
-        Button(onClick = { },
-            modifier = Modifier.padding(10.dp)) {
-            Text(text = "boton2")
+        Spacer(Modifier.weight(0.5f, true))
+        IconButton(onClick = { /*TODO*/ }) { // (3)
+            Icon(imageVector = Icons.Filled.Home, contentDescription = "Buscar")
         }
-        Button(onClick = { },
-            modifier = Modifier.padding(10.dp)) {
-            Text(text = "boton3")
+        Spacer(Modifier.weight(0.5f, true))
+        IconButton(onClick = { /*TODO*/ }) { // (3)
+            Icon(imageVector = Icons.Filled.Home, contentDescription = "Buscar")
         }
+        Spacer(Modifier.weight(0.5f, true))
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun Wrapper (modifier: Modifier = Modifier){
+fun Wrapper(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         TitleContainer()
         Column(modifier = modifier.height(400.dp)) {
             ListContainer()
         }
-        ButtonContainer()
+        BottomContainer()
     }
 }
