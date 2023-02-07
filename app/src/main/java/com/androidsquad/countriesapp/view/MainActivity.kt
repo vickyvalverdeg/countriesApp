4918f87c2c19e09866b41696503f9398c15588ff
hernan.cid <hernan.cid@thoughtworks.com>
2023-02-07 14:44:44 -0300

    Code working with both changes

--- app/src/main/java/com/androidsquad/countriesapp/view/MainActivity.kt
+++ app/src/main/java/com/androidsquad/countriesapp/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -13,9 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -23,26 +22,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.androidsquad.countriesapp.R
 import com.androidsquad.countriesapp.model.Country
 import com.androidsquad.countriesapp.viewModel.MainViewModel
 
@@ -206,92 +193,8 @@ fun ItemContainer(model: Country) {
 }
 
 
-@Composable
-fun MovieList(movieList: List<Movie>) {
-    var selectedIndex by remember { mutableStateOf(-1) }
-    LazyColumn {
-
-        itemsIndexed(items = movieList) { index, item ->
-            MovieItem(movie = item, index, selectedIndex) { i ->
-                selectedIndex = i
-            }
-        }
-    }
-
-}
-
-@Composable
-fun MovieItem(movie: Movie, index: Int, selectedIndex: Int, onClick: (Int) -> Unit) {
-
-    val backgroundColor =
-        if (index == selectedIndex) MaterialTheme.colors.primary else MaterialTheme.colors.background
-    Card(
-        modifier = Modifier
-            .padding(8.dp, 4.dp)
-            .fillMaxWidth()
-            .clickable { onClick(index) }
-            .height(110.dp), shape = RoundedCornerShape(8.dp), elevation = 4.dp
-    ) {
-        Surface(color = backgroundColor) {
-
-            Row(
-                Modifier
-                    .padding(4.dp)
-                    .fillMaxSize()
-            ) {
-
-                Image(
-                    painter = rememberImagePainter(
-                        data = movie.imageUrl,
-
-                        builder = {
-                            scale(Scale.FILL)
-//                            placeholder(R.drawable)
-                            transformations(CircleCropTransformation())
-
-                        }
-                    ),
-                    contentDescription = movie.desc,
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .weight(0.2f)
-                )
 
 
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxHeight()
-                        .weight(0.8f)
-                ) {
-                    Text(
-                        text = movie.name,
-                        style = MaterialTheme.typography.subtitle1,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = movie.category,
-                        style = MaterialTheme.typography.caption,
-                        modifier = Modifier
-                            .background(
-                                Color.LightGray
-                            )
-                            .padding(4.dp)
-                    )
-                    Text(
-                        text = movie.desc,
-                        style = MaterialTheme.typography.body1,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
-                    )
-
-                }
-            }
-        }
-    }
-
-}
 
 
 @Preview(showBackground = true)
