d0fc440ddfda290e004888e01d55cdd3bd95e270
CarlosTrujilloTW <carlos.trujillo@thoughtworks.com>
2023-02-09 15:03:03 -0300

    remove night themes and add navigation between activities

--- app/src/main/java/com/androidsquad/countriesapp/view/MainActivity.kt
+++ app/src/main/java/com/androidsquad/countriesapp/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.androidsquad.countriesapp.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Window
 import android.widget.Toast
@@ -28,6 +29,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import com.androidsquad.countriesapp.R
 import com.androidsquad.countriesapp.model.Country
 import com.androidsquad.countriesapp.viewModel.MainViewModel
@@ -64,7 +66,8 @@ fun TopBarContainer() {
         backgroundColor = colorResource(id = R.color.background_top_bar),
         actions = {
             IconButton(onClick = {
-                Toast.makeText(context, " account clicked!", Toast.LENGTH_SHORT).show()
+
+                //Toast.makeText(context, " account clicked!", Toast.LENGTH_SHORT).show()
             }) {
                 Image(
                     painter = painterResource(id = R.drawable.account_icon),
@@ -165,9 +168,7 @@ fun ItemContainer(model: Country) {
             .padding(8.dp)
             .fillMaxWidth()
             .clickable {
-                Toast
-                    .makeText(context, "clicked on: " + model.name, Toast.LENGTH_SHORT)
-                    .show()
+                context.startActivity(Intent(context, CountriesActivity::class.java).putExtra("continent",model.name))
             },
         elevation = 4.dp,
         shape = RoundedCornerShape(size = 12.dp)
