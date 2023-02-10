1f9b9608e16ce0baef3e4b4ee7b2d58d835b9b87
CarlosTrujilloTW <carlos.trujillo@thoughtworks.com>
2023-02-10 00:29:26 -0300

    Se crea dataclass para continent, se agregan atributos a Country, se configura las card con data dummy y disposicion de elementos segun template de figma, tambien se agrega boton volver al topbar

--- app/src/main/java/com/androidsquad/countriesapp/model/Country.kt
+++ app/src/main/java/com/androidsquad/countriesapp/model/Country.kt
@@ -2,5 +2,8 @@ package com.androidsquad.countriesapp.model
 
 data class Country(
     val name: String,
+    val capital: String,
+    val language: String,
+    val currency: String,
     val image:Int
 )
